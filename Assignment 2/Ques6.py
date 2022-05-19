a54da1e6e4252ca4d39f409b74c46128bf00909a
kuber21107053 <104064783+kuber21107053@users.noreply.github.com>
2022-05-19 17:26:19 +0530

    Update Ques6.py

--- Assignment 2/Ques6.py
+++ Assignment 2/Ques6.py
@@ -6,7 +6,7 @@ number_2=int(input())
 
 ex_or=number_1^number_2 #to know which bits are different as they will give output 1
 bin_exor=bin(ex_or)
-check_string=str(ex_or)
+check_string=str(bin_exor)
 
 a=check_string.count("1")
 print(a)
